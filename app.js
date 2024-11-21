54bda365d6db8c463356733a636320add571b6b5
Maite <anamaitegarciacorrea@gmail.com>
2024-11-21 22:51:21 +0100

    Añado el desplegable de FAQs

--- app.js
+++ app.js
@@ -87,3 +87,22 @@ leftBtn.addEventListener("click",()=>{
 
 //Inicializar el carrusel
 updateCarousel();
+
+
+//ACORDEÓN FAQS
+const blocks=document.querySelectorAll(".block");
+const acTitles=document.querySelectorAll(".ac-title");
+
+//Cuando click en h2 se quita la clase ACTIVE de todos los bloques y después
+//vamos a añadir la clase activo al bloque con la posición del h2
+
+acTitles.forEach((title,index) => {
+    acTitles[index].addEventListener("click",()=>{
+
+        blocks.forEach((block,i)=>{
+            blocks[i].classList.remove("active");
+        })
+
+        blocks[index].classList.add("active");
+    })
+});
