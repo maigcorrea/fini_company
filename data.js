665e1134ea675f47e1f59295f6b3d90d29fee16a
Maite <anamaitegarciacorrea@gmail.com>
2024-11-24 22:10:50 +0100

    Inserto los productos en la página desde JS mediate un JSON pasado a JS

--- data.js
+++ data.js
@@ -1,35 +1,40 @@
-{
-    "productos":[
+
+    let productos=[
         {
             "id": 1,
+            "image":"./img/p1_Chocogang-marsh.png",
             "nombre":"ChocoGang Marshmallow",
             "gramos":"35g"
         },
         {
             "id": 2,
+            "image":"./img/p1_variation_Chocogang-tubes.png",
             "nombre":"ChocoGang Tubes",
             "gramos":"40g"
         },
         {
             "id": 3,
+            "image":"./img/party-mix-12-unidades-removebg-preview.png",
             "nombre":"Party Mix",
             "gramos":"90g"
         },
         {
             "id": 4,
+            "image":"./img/moras-removebg-preview.png",
             "nombre":"Jelly Berries Moras",
             "gramos":"90g"
         },
         {
             "id": 5,
+            "image":"./img/mellows-popcorn-web-1-1-1-removebg-preview.png",
             "nombre":"The Mellows Sweet PopCorn",
             "gramos":"80g"
         },
         {
             "id": 6,
+            "image":"./img/p6_MELLOWS-MAGIC.png",
             "nombre":"The Mellows Magic",
             "gramos":"80g"
         }
     ]
         
-}
