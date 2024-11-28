3085aaa30c08aab91deb84950f73476b69320b4d
Maite <anamaitegarciacorrea@gmail.com>
2024-11-28 19:58:42 +0100

    Hago y doy funcionalidad a la sección de buscar puesto de trabajo

---
+++ JS/dataPositions.js
@@ -0,0 +1,26 @@
+let positions=[
+    {
+        "id": 1,
+        "title":"Sales Data Analyst Junior",
+        "location":"Molina de Segura, España",
+        "type":"Presencial"
+    },
+    {
+        "id": 2,
+        "title":"Analista de procesos logísticos y datos",
+        "location":"Molina de Segura, España",
+        "type":"Teletrabajo"
+    },
+    {
+        "id": 3,
+        "title":"Ingeniero/a de automatización",
+        "location":"Molina de Segura, España",
+        "type":"Presencial"
+    },
+    {
+        "id": 4,
+        "title":"Mozo/a de almacén",
+        "location":"Molina de Segura, España",
+        "type":"Presencial"
+    }
+]
