3dea1bb6c105d6e995a0d7df688976933ba1bf20
Maite <anamaitegarciacorrea@gmail.com>
2024-11-29 18:03:16 +0100

    Agrego algunos detalles de hover

--- JS/app.js
+++ JS/app.js
@@ -84,6 +84,9 @@
 
 const menuIcono=document.querySelector(".menuIcono p");
 const header=document.querySelector("header");
+const prueba=document.querySelectorAll("nav>div:nth-child(2)  ul a");
+console.log(prueba);
+console.log("hola");
 
 menuIcono.addEventListener("click",()=>{
   if(menuIcono.textContent=="☰"){
